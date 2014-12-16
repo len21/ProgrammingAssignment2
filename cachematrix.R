81247e1e90170180ef8f59fdd0e1fb9eb60a382f
len21 <moiralennox@gmail.com>
2014-12-15 19:53:52 -0600

    update the script with 2 function for matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  # set x = empty matric
+  # set inverse variable to null
+  mtxInverse <- NULL
+  
+  # Create a new method called "set" that set the value of the matrix
+  set<-function(y){
+    x <<- y
+    # set function assigns the argument to x
+    mtxInverse <<- NULL
+    # reset Inverse cto NULL
+  }
+  
+  # Create a new method called "get" that get the value of the matrix
+  get <- function() x
+  
+  # Create a new method called "setmatrix" that get the inverse of variable mtxInverse
+  setmatrixInverse<-function(solve)  mtxInverse <<- solve
+  
+  # Create a new method called "getmatrix" which returns the inverse matrix
+  getmatrixInverse <- function() mtxInverse
+  
+  # return a list with the four methods/functions
+  list(set = set, get = get, setmatrixInverse = setmatrixInverse, getmatrixInverse = getmatrixInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# calculates the mean of the special "matrix" created with the in the function makeCacheMatrix()
+cacheSolve <- function(x=matrix(), ...) {
+  
+  # get the value of the inverse
+  mtxInverse <- x$getmatrixInverse()
+  
+  # If the value of Inverse is NOT null returns that value 
+  if(!is.null(mtxInverse)){
+    message("getting cached data")
+    return(mtxInverse)
+  }
+  
+  # since mtxInverse is null (aka not cached) set data = to the value of x and calculate the inverse
+  data <- x$get()
+  
+  # set mtxInverse = Inverse of x
+  mtxInverse <- solve(data, ...)
+  
+  # set the new Inverse value to cache
+  x$setmatrixInverse(mtxInverse)
+  
+  # Returns the new Inverse value
+  mtxInverse 
+  
 }
